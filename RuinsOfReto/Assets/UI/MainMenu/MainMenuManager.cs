1ec3f4838d0c7e6898086ededfe74878f3c3317d
Hossein Panahloo <hossein.panahloo@gmail.com>
2020-07-12 03:16:50 +0430

    SUMMARY: Added: Music Engine Added TestSound Scene for Test music fadein and fadeouts
    DESCRIPTION:
    Used temprory musics for calm game zone
    
    Changes:
    
    Broken things:

---
+++ RuinsOfReto/Assets/UI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicEngine.Instance.Play_MusicSituation(MusicEngine.Music_Situation.Start_MenuScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
